fc2f26433487ea893901a2b60ace66181ce7c53a
animmore <zaskokazhenya@gmail.com>
2019-09-25 22:32:02 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I am Yevhenii Chut. I want to become a React Native Developer! 
         </p>
         <a
           className="App-link"
